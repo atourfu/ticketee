51c5f04fb780d2a0425a100c93446304e44b9efe
atourfu <atourfu@gmail.com>
2016-03-08 23:57:43 +0800

    Implement deleting tickets feature

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -30,7 +30,14 @@ class TicketsController < ApplicationController
 	    
 	    render action: "edit"
 	  end
-	end		
+	end
+
+	def destroy
+	  @ticket.destroy
+	  flash[:notice] = "Ticket has been deleted."
+	  
+	  redirect_to @project
+	end	
 
 	private
 
