59439c4129b153575b6fb5c47c0798594baab153
atourfu <atourfu@gmail.com>
2016-03-09 15:09:00 +0800

    Implemented signing in.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get '/signin', to: "sessions#new"
+  post '/signin', to: "sessions#create"
+  
   root "projects#index"
 
   resources :projects do
--- spec/factories/user_factory.rb
+++ spec/factories/user_factory.rb
@@ -1,5 +1,6 @@
 FactoryGirl.define do
 	factory :user do
+		name "steve"
 		password "password"
 		password_confirmation "password"
 		email "user@example.com"
