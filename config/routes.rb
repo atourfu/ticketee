e1b44be863966087388d7512650a08bfc360e249
atourfu <atourfu@gmail.com>
2016-03-15 23:52:11 +0800

    Add permissions screen for admins

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,17 @@
 Rails.application.routes.draw do
   namespace :admin do
     root :to => "base#index"
-    resources :users
+    resources :users do
+      resources :permissions
+
+      put "permissions", to: "permissions#set",
+                          as: "set_permissions"
+    end
   end
 
   get '/signin', to: "sessions#new"
   post '/signin', to: "sessions#create"
+  delete "/signout", to: "sessions#destroy", as: "signout"
   
   root "projects#index"
 
