dd2f8b0549048c42d914030cc248cab07db13943
atourfu <atourfu@gmail.com>
2016-03-19 00:16:20 +0800

    Only users with the 'change states' permission can change states

--- app/helpers/admin/permissions_helper.rb
+++ app/helpers/admin/permissions_helper.rb
@@ -4,7 +4,8 @@ module Admin::PermissionsHelper
 			"view" => "View",
 			"create tickets" => "Create Tickets",
 			"edit tickets" => "Edit Tickets",
-    		"delete tickets" => "Delete Tickets"
-		}
-	end
+      "delete tickets" => "Delete Tickets",
+      "change states" => "Change States"
+    }
+  end
 end
--- spec/features/admin/assigning_permissions_spec.rb
+++ spec/features/admin/assigning_permissions_spec.rb
@@ -8,6 +8,7 @@ feature "Assigning permissions" do
 		user: user) }
 
 	before do
+		State.create!(:name => "Open")
 		sign_in_as!(admin)
 
 		click_link "Admin"
@@ -27,49 +28,66 @@ feature "Assigning permissions" do
 	end
 
 	scenario "Creating tickets for a project" do
-	  check_permission_box "view", project
-	  check_permission_box "create_tickets", project
-	  click_button "Update"
-	  click_link "Sign out"
-	  
-	  sign_in_as!(user)
-	  click_link project.name
-	  click_link "New Ticket"
-	  fill_in "Title", with: "Shiny!"
-	  fill_in "Description", with: "Make it so!"
-	  click_button "Create"
-	  
-	  expect(page).to have_content("Ticket has been created.")
+		check_permission_box "view", project
+		check_permission_box "create_tickets", project
+		click_button "Update"
+		click_link "Sign out"
+
+		sign_in_as!(user)
+		click_link project.name
+		click_link "New Ticket"
+		fill_in "Title", with: "Shiny!"
+		fill_in "Description", with: "Make it so!"
+		click_button "Create"
+
+		expect(page).to have_content("Ticket has been created.")
 	end
 
 	scenario "Updating a ticket for a project" do
-	  check_permission_box "view", project
-	  check_permission_box "edit_tickets", project
-	  click_button "Update"
-	  click_link "Sign out"
-	  
-	  sign_in_as!(user)
-	  click_link project.name
-	  click_link ticket.title
-	  click_link "Edit Ticket"
-	  fill_in "Title", with: "Really shiny!"
-	  click_button "Update Ticket"
-	  
-	  expect(page).to have_content("Ticket has been updated")
+		check_permission_box "view", project
+		check_permission_box "edit_tickets", project
+		click_button "Update"
+		click_link "Sign out"
+
+		sign_in_as!(user)
+		click_link project.name
+		click_link ticket.title
+		click_link "Edit Ticket"
+		fill_in "Title", with: "Really shiny!"
+		click_button "Update Ticket"
+
+		expect(page).to have_content("Ticket has been updated")
 	end
 
 	scenario "Deleting a ticket for a project" do
-	  check_permission_box "view", project
-	  check_permission_box "delete_tickets", project
-	  
-	  click_button "Update"
-	  click_link "Sign out"
-	  
-	  sign_in_as!(user)
-	  click_link project.name
-	  click_link ticket.title
-	  click_link "Delete Ticket"
-	  
-	  expect(page).to have_content("Ticket has been deleted.")
+		check_permission_box "view", project
+		check_permission_box "delete_tickets", project
+
+		click_button "Update"
+		click_link "Sign out"
+
+		sign_in_as!(user)
+		click_link project.name
+		click_link ticket.title
+		click_link "Delete Ticket"
+
+		expect(page).to have_content("Ticket has been deleted.")
+	end
+
+	scenario "Changing states for a ticket" do
+		check_permission_box "view", project
+		check_permission_box "change_states", project
+		click_button "Update"
+		click_link "Sign out"
+		sign_in_as!(user)
+		click_link project.name
+		click_link ticket.title
+		fill_in "Text", :with => "Opening this ticket."
+		select "Open", :from => "State"
+		click_button "Create Comment"
+		page.should have_content("Comment has been created.")
+		within("#ticket .state") do
+			page.should have_content("Open")
+		end
 	end
 end
